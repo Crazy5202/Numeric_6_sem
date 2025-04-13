2e4f3f1011641f0543295f261509d36f2359b318
maxim <mdkutsenko@mai.education>
2025-04-13 17:02:43 +0300

    Added drawing to 3.1

--- Lab3/Files/3_1.py
+++ Lab3/Files/3_1.py
@@ -1,9 +1,10 @@
 import os
 import math
+from matplotlib import pyplot as plt
 
 class NUMERIC_APPROX:
     def __init__(self):
-        self.arg = 0.8
+        self.arg = 0.0
         self.a = []
         self.b = []
 
@@ -35,7 +36,9 @@ class NUMERIC_APPROX:
                 break
         return index
 
-    def calc_lagr(self, order):
+    def calc_lagr(self, order, argument = None):
+        if argument is None:
+            argument = self.arg
         sum = []
         for arr in [self.a, self.b]:
             if (order > len(arr)-1):
@@ -50,12 +53,13 @@ class NUMERIC_APPROX:
                     if i==j:
                         continue
                     cur_val /= arr[i] - arr[j]
-                    cur_val *= self.arg - arr[j]
+                    cur_val *= argument - arr[j]
                 cur_sum += cur_val
             sum.append(cur_sum)
         print(f"\nЛагранж степени {order}:")
         for i in range (len(sum)):
-            print(f"Погрешность для значений {i+1}: {abs(sum[i] - self.calc_eq(self.arg))}")
+            print(f"Погрешность для значений {i+1}: {abs(sum[i] - self.calc_eq(argument))}")
+        return sum[0]
 
     def razd_razn_iter(self, array, left, right):
         if (left==right): return self.calc_eq(array[left])
@@ -63,7 +67,9 @@ class NUMERIC_APPROX:
         right_val = self.razd_razn_iter(array, left+1, right)
         return (left_val - right_val) / (array[left] - array[right])
     
-    def calc_newton(self, order):
+    def calc_newton(self, order, argument = None):
+        if argument is None:
+            argument = self.arg
         sum = []
         for arr in [self.a, self.b]:
             if (order > len(arr)-1):
@@ -75,18 +81,58 @@ class NUMERIC_APPROX:
             for i in range (pos, pos+order+1):
                 cur_val = self.razd_razn_iter(arr, pos, i)
                 for j in range (pos, i):
-                    cur_val *= (self.arg - arr[j])
+                    cur_val *= (argument - arr[j])
                 cur_sum += cur_val
             sum.append(cur_sum)
         print(f"\nНьютон степени {order}:")
         for i in range (len(sum)):
-            print(f"Погрешность для значений {i+1}: {abs(sum[i] - self.calc_eq(self.arg))}")
+            print(f"Погрешность для значений {i+1}: {abs(sum[i] - self.calc_eq(argument))}")
+        return sum[0]
 
 if __name__ == "__main__":
     solver = NUMERIC_APPROX()
 
     solver.read_from_file("input_1.txt")
+    
     solver.calc_lagr(2)
     solver.calc_lagr(3)
+
     solver.calc_newton(2)
     solver.calc_newton(3)
+
+    """
+    split = 100
+    left_edge = solver.a[0]
+    right_edge = solver.a[-1]
+        
+    x = [left_edge + (right_edge-left_edge)/split*i for i in range (split)]
+
+    func_values = [solver.calc_eq(elem) for elem in x]
+    plt.plot(x, func_values, label='Функция')
+
+    values = [solver.calc_lagr(2, elem) for elem in x]
+    plt.plot(x, values, label = 'Лагранж 2')
+
+    values = [solver.calc_lagr(3, elem) for elem in x]
+    plt.plot(x, values, label = 'Лагранж 3')
+
+    plt.title("Приближение Лагранжем.")
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+    
+
+    plt.plot(x, func_values, label='Функция')
+
+    values = [solver.calc_newton(2, elem) for elem in x]
+    plt.plot(x, values, label = 'Ньютон 2')
+
+    values = [solver.calc_newton(3, elem) for elem in x]
+    plt.plot(x, values, label = 'Ньютон 3')
+
+    plt.title("Приближение Ньютоном.")
+    plt.grid()
+    plt.legend()
+    plt.show()
+    """
